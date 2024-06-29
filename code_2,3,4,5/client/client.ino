8a035f073688fac94345cd8c7191fc1bfaaeaa47
adel4780 <adelk138019@gmail.com>
2024-06-29 09:55:07 +0330

    add Client

---
+++ code_2,3,4,5/client/client.ino
@@ -0,0 +1,69 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <WiFiClientSecure.h>
+#include <ArduinoJson.h>
+
+const char* ssid = "Tartarous";
+const char* password = "ajhv6004";
+const char* host = "https://danial.pythonanywhere.com/getStatus";
+
+HTTPClient http;
+WiFiClientSecure client2;
+int ledPin = 4;
+String status = "OFF";
+int time_on = 0;
+int time_off = 0;
+
+void checkServer() {
+  client2.setInsecure(); // Use with caution, this disables SSL verification
+  http.begin(client2, host); // Initialize HTTP with SSL client
+  int httpCode = http.GET();
+  if (httpCode == 200) {
+    String payload = http.getString();
+    // Serial.println(payload);
+    DynamicJsonDocument doc(1024);
+    deserializeJson(doc, payload);
+    status = doc["status"].as<String>();
+    time_on = doc["scheduling"]["time_on"];
+    time_off = doc["scheduling"]["time_off"];
+  } else {
+    Serial.printf("Error on HTTP request, code: %d\n", httpCode);
+  }
+  http.end();
+}
+
+void setup() {
+  Serial.begin(115200);
+  delay(10);
+  pinMode(ledPin, OUTPUT);
+
+  // Connect to WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+}
+
+void loop() {
+  if (status == "OFF") {
+    digitalWrite(ledPin, LOW);
+  }
+  if (status == "ON") {
+    digitalWrite(ledPin, HIGH);
+  }
+  if (status == "Blink") {
+    digitalWrite(ledPin, HIGH);
+    delay(time_on);
+    digitalWrite(ledPin, LOW);
+    delay(time_off);
+  }
+  Serial.printf("Status: %s, time_ON: %d ms, time_OFF: %d ms\n", status.c_str(), time_on, time_off);
+  delay(10); // Wait 5 seconds before checking the server again
+  checkServer();
+}
