4fde95e28813c1c721616ec48716b0003a71ea03
adel4780 <adelk138019@gmail.com>
2024-06-29 07:49:54 +0330

    edit

--- Django-Server-IoT/iotproject/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-from .models import values
-
-admin.site.register(values)
--- Django-Server-IoT/iotproject/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-class values(models.Model):
-    timeOn = models.IntegerField()
-    timeOff = models.IntegerField()
